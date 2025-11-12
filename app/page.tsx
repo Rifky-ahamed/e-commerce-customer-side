36e50ee72944e3f8d6014a097db09cec60153da6
Rifky-ahamed <rifkyr099@gmail.com>
2025-11-12 12:13:08 +0530

    Add success message for Next.js project in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <h1 className="text-4xl font-bold text-green-600">Tailwind CSS is Working ✅</h1>
+      <h1 className="text-4xl font-bold text-green-600">Next.js project is Working ✅</h1>
     </div>
   );
 }
